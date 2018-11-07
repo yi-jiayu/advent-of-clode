6f826034c4c86cf28a4352913e94d3f449c0182f
Jiayu Yi <jiayu.yi@thoughtworks.com>
2018-11-07 22:18:18 +0800

    Finish day 2 part 1

---
+++ src/aoc2015/day02.clj
@@ -0,0 +1,23 @@
+(ns aoc2015.day02
+    (:require [clojure.string :refer [split split-lines]]))
+
+(defn parse-lwh [s]
+  (map #(Integer/parseInt %) (split s #"x")))
+
+(defn parse-input [input]
+  (let [lines (split-lines input)]
+       (map parse-lwh lines)))
+
+(defn surface-area [lwh]
+  (let [[l w h] lwh]
+    (* 2 (+ (* l w) (* w h) (* h l)))))
+
+(defn smallest-side [lwh]
+  (let [[l w h] lwh]
+    (min (* l w) (* w h) (* h l))))
+
+(defn wrapping-paper-required [lwh]
+  (+ (surface-area lwh) (smallest-side lwh)))
+
+(defn total-wrapping-paper-required [presents]
+  (apply + (map wrapping-paper-required presents)))
---
+++ test/aoc2015/day02_test.clj
@@ -0,0 +1,27 @@
+(ns aoc2015.day02-test
+  (:require [clojure.test :refer :all]
+            [clojure.java.io :as io]
+            [aoc2015.day02 :refer :all]))
+
+(def input (slurp (io/resource "day02.txt")))
+
+(deftest test-parse-lwh
+  (is (= '(2 3 4) (parse-lwh "2x3x4"))))\
+
+(deftest test-parse-input
+  (is (= '('(2 3 4) '(1 1 10) (parse-input "2x3x4\n1x1x10")))))
+
+(deftest test-surface-area
+  (is (= 52 (surface-area '(2 3 4))))
+  (is (= 42 (surface-area '(1 1 10)))))
+
+(deftest test-smallest-side
+  (is (= 6 (smallest-side '(2 3 4))))
+  (is (= 1 (smallest-side '(1 1 10)))))
+
+(deftest test-wrapping-paper-required
+  (is (= 58 (wrapping-paper-required '(2 3 4))))
+  (is (= 43 (wrapping-paper-required '(1 1 10)))))
+
+(deftest test-total-wrapping-paper-required
+  (is (= 1606483 (total-wrapping-paper-required (parse-input input)))))
