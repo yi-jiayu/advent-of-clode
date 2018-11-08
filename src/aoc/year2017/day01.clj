818a4eb5f6874153570e881ec6f96c429c7f4eb3
Jiayu Yi <jiayu.yi@thoughtworks.com>
2018-11-08 10:13:07 +0800

    Refactor year 2015 day 1 part 1 solution

--- src/aoc/year2017/day01.clj
+++ src/aoc/year2017/day01.clj
@@ -3,4 +3,5 @@
 (defn solve-captcha [captcha]
   (let [captcha (str captcha (first captcha))]
     (let [pairs (partition 2 1 captcha)]
-      (apply + (map (comp #(Character/digit % 10) first) (filter #(let [[a b] %] (identical? a b)) pairs))))))
+      (apply + (map #(Character/digit (first %) 10)
+                    (filter #(apply identical? %) pairs))))))
