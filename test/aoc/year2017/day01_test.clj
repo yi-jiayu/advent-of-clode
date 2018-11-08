4a379153955fe6f7d92c93364c801d6d0d1290e0
Jiayu Yi <jiayu.yi@thoughtworks.com>
2018-11-08 10:40:07 +0800

    Add solution for year 2017 day 1 part 2

--- test/aoc/year2017/day01_test.clj
+++ test/aoc/year2017/day01_test.clj
@@ -5,11 +5,21 @@
 
 (def input (slurp (io/resource "year2017/day01.txt")))
 
-(deftest test-solve-captcha
+(deftest test-solve-captcha-part-1
   (testing "example inputs"
-    (is (= 3 (solve-captcha "1122")))
-    (is (= 4 (solve-captcha "1111")))
-    (is (= 0 (solve-captcha "1234")))
-    (is (= 9 (solve-captcha "91212129"))))
+    (is (= 3 (solve-captcha-part-1 "1122")))
+    (is (= 4 (solve-captcha-part-1 "1111")))
+    (is (= 0 (solve-captcha-part-1 "1234")))
+    (is (= 9 (solve-captcha-part-1 "91212129"))))
   (testing "actual input"
-    (is (= 1253 (solve-captcha input)))))
+    (is (= 1253 (solve-captcha-part-1 input)))))
+
+(deftest test-solve-captcha-part-2
+  (testing "example inputs"
+    (is (= 6 (solve-captcha-part-2 "1212")))
+    (is (= 0 (solve-captcha-part-2 "1221")))
+    (is (= 4 (solve-captcha-part-2 "123425")))
+    (is (= 12 (solve-captcha-part-2 "123123")))
+    (is (= 4 (solve-captcha-part-2 "12131415"))))
+  (testing "actual input"
+    (is (= 1278 (solve-captcha-part-2 input)))))
