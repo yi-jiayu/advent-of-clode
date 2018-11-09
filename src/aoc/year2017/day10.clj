9fd911e9d831a31f6fd9fec91f73b5d070f50dff
Jiayu Yi <jiayu.yi@thoughtworks.com>
2018-11-09 23:10:28 +0800

    aoc.year2017.day10/reverse-sublist: Support sublists which wrap around

--- src/aoc/year2017/day10.clj
+++ src/aoc/year2017/day10.clj
@@ -1,9 +1,21 @@
 (ns aoc.year2017.day10)
 
+; rotate function from https://groups.google.com/forum/#!topic/clojure/SjmevTjZPcQ
+(defn rotate
+  "Take a collection and left rotates it n steps.
+  If n is negative, the collection is rotated right. Executes in O(n) time."
+  [n coll]
+  (let [c (count coll)]
+    (take c (drop (mod n c) (cycle coll)))))
+
 (defn reverse-sublist
   "Reverses the `length` element long sublist of `l` starting from the element at index `start`."
   [l start length]
-  (let [bef (subvec l 0 start)
-        mid (subvec l start (+ start length))
-        aft (subvec l (+ start length))]
-    (into [] (concat bef (reverse mid) aft))))
+  (if (< length 2)
+    ; lengths of 0 or 1 have no effect
+    l
+    (let [l' (into [] (rotate start l))
+          mid (subvec l' 0 length)
+          aft (subvec l' length)
+          l'' (into [] (concat (reverse mid) aft))]
+      (rotate (- start) l''))))
--- test/aoc/year2017/day10_test.clj
+++ test/aoc/year2017/day10_test.clj
@@ -3,4 +3,7 @@
   (:require [aoc.year2017.day10 :refer :all]))
 
 (deftest test-reverse-sublist
-  (is (= [2 1 0 3 4] (reverse-sublist [0 1 2 3 4] 0 3))))
+  (is (= [2 1 0 3 4] (reverse-sublist [0 1 2 3 4] 0 3)))
+  (is (= [4 3 0 1 2] (reverse-sublist [2 1 0 3 4] 3 4)))
+  (is (= [4 3 0 1 2] (reverse-sublist [4 3 0 1 2] 1 1)))
+  (is (= [4 3 0 1 2] (reverse-sublist [3 4 2 1 0] 1 5))))
