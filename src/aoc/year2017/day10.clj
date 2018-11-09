1382be698f7a97a792caf0b19989c2b0c29e7adc
Jiayu Yi <jiayu.yi@thoughtworks.com>
2018-11-09 23:10:56 +0800

    aoc.year2017.day10: Finish part 1 solution

--- src/aoc/year2017/day10.clj
+++ src/aoc/year2017/day10.clj
@@ -19,3 +19,25 @@
           aft (subvec l' length)
           l'' (into [] (concat (reverse mid) aft))]
       (rotate (- start) l''))))
+
+(defn knot-hash-update
+  "Updates `l` based on `curr-pos`, `skip-size` and `length`. Returns a tuple of the new l, curr-pos and skip-size."
+  [[l curr-pos skip-size] length]
+  (let [l' (reverse-sublist l curr-pos length)
+        curr-pos' (rem (+ curr-pos length skip-size) (count l))
+        skip-size' (+ skip-size 1)]
+    [l' curr-pos' skip-size']))
+
+(defn knot-hash
+  "This hash function simulates tying a knot in a circle of string with 256 marks on it
+Based on the input to be hashed, the function repeatedly selects a span of string, brings the ends together, and gives
+the span a half-twist to reverse the order of the marks within it. After doing this many times, the order of the marks
+is used to build the resulting hash."
+  ([lengths] (knot-hash 256 lengths))
+  ([max-length lengths]
+   (let [l (into [] (range max-length))
+         l' (first (reduce knot-hash-update [l 0 0] lengths))]
+     (apply * (take 2 l')))))
+
+(defn parse-input [input]
+  (map #(Integer/parseInt %) (clojure.string/split input #",")))
--- test/aoc/year2017/day10_test.clj
+++ test/aoc/year2017/day10_test.clj
@@ -2,8 +2,23 @@
   (:require [clojure.test :refer :all])
   (:require [aoc.year2017.day10 :refer :all]))
 
+(def input "197,97,204,108,1,29,5,71,0,50,2,255,248,78,254,63")
+
 (deftest test-reverse-sublist
   (is (= [2 1 0 3 4] (reverse-sublist [0 1 2 3 4] 0 3)))
   (is (= [4 3 0 1 2] (reverse-sublist [2 1 0 3 4] 3 4)))
   (is (= [4 3 0 1 2] (reverse-sublist [4 3 0 1 2] 1 1)))
   (is (= [4 3 0 1 2] (reverse-sublist [3 4 2 1 0] 1 5))))
+
+(deftest test-knot-hash-update
+  (is (= [[2 1 0 3 4] 3 1] (knot-hash-update [[0 1 2 3 4] 0 0] 3)))
+  (is (= [[4 3 0 1 2] 3 2] (knot-hash-update [[2 1 0 3 4] 3 1] 4)))
+  (is (= [[4 3 0 1 2] 1 3] (knot-hash-update [[4 3 0 1 2] 3 2] 1)))
+  (is (= [[3 4 2 1 0] 4 4] (knot-hash-update [[4 3 0 1 2] 1 3] 5))))
+
+(deftest test-knot-hash
+  (is (= 12 (knot-hash 5 [3 4 1 5])))
+  (is (= 40132 (knot-hash (parse-input input)))))
+
+(deftest test-parse-input
+  (is (= [3 4 1 5] (parse-input "3,4,1,5"))))
