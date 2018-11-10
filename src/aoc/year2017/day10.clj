f551a79eebf2e76a5d03adbd2567561ac00e79e5
Jiayu Yi <jiayu.yi@thoughtworks.com>
2018-11-10 12:06:50 +0800

    aoc.year2017.day10: Finish part 2 solution

--- src/aoc/year2017/day10.clj
+++ src/aoc/year2017/day10.clj
@@ -1,5 +1,9 @@
 (ns aoc.year2017.day10)
 
+(def standard-length-suffix-values [17 31 73 47 23])
+(def max-length 256)
+(def num-rounds 64)
+
 ; rotate function from https://groups.google.com/forum/#!topic/clojure/SjmevTjZPcQ
 (defn rotate
   "Take a collection and left rotates it n steps.
@@ -28,16 +32,48 @@
         skip-size' (+ skip-size 1)]
     [l' curr-pos' skip-size']))
 
+(defn knot-hash-round
+  "Applies one round of knot-tying and rearranging marks to `l` based on `lengths`."
+  ([lengths l curr-pos skip-size]
+   (reduce knot-hash-update [l curr-pos skip-size] lengths)))
+
+(defn parse-input [input]
+  (map #(Integer/parseInt %) (clojure.string/split input #",")))
+
+(defn parse-input-bytes
+  "Parse the input as a byte string, returning a vector of lengths between 0 and 255."
+  [input]
+  (map int input))
+
+(defn knot-hash-rounds
+  "Runs 64 rounds of knot-tying and rearranging marks according to `lengths` and returns the final order."
+  [lengths]
+  (let [lengths (into [] (concat lengths standard-length-suffix-values))]
+    (loop [rounds-remaining num-rounds
+           [l curr-pos skip-size] [(into [] (take max-length (range))) 0 0]]
+      (if (> rounds-remaining 0)
+        (recur (- rounds-remaining 1)
+               (knot-hash-round lengths l curr-pos skip-size))
+        l))))
+
+(defn condense-hash
+  "Condenses the sparse hash obtained after running the knot hash rounds into a shorter dense hash."
+  [l]
+  (map (partial apply bit-xor) (partition 16 l)))
+
+(defn to-hex
+  "Converts the condensed hash into its hexadecimal representation."
+  [condensed-hash]
+  (apply str (map (partial format "%02x") condensed-hash)))
+
 (defn knot-hash
   "This hash function simulates tying a knot in a circle of string with 256 marks on it
 Based on the input to be hashed, the function repeatedly selects a span of string, brings the ends together, and gives
 the span a half-twist to reverse the order of the marks within it. After doing this many times, the order of the marks
 is used to build the resulting hash."
-  ([lengths] (knot-hash 256 lengths))
-  ([max-length lengths]
-   (let [l (into [] (range max-length))
-         l' (first (reduce knot-hash-update [l 0 0] lengths))]
-     (apply * (take 2 l')))))
-
-(defn parse-input [input]
-  (map #(Integer/parseInt %) (clojure.string/split input #",")))
+  [input]
+  (-> input
+      parse-input-bytes
+      knot-hash-rounds
+      condense-hash
+      to-hex))
--- test/aoc/year2017/day10_test.clj
+++ test/aoc/year2017/day10_test.clj
@@ -16,9 +16,36 @@
   (is (= [[4 3 0 1 2] 1 3] (knot-hash-update [[4 3 0 1 2] 3 2] 1)))
   (is (= [[3 4 2 1 0] 4 4] (knot-hash-update [[4 3 0 1 2] 1 3] 5))))
 
-(deftest test-knot-hash
-  (is (= 12 (knot-hash 5 [3 4 1 5])))
-  (is (= 40132 (knot-hash (parse-input input)))))
+(deftest test-knot-hash-round
+  (is (= 12 (apply * (take 2 (first (knot-hash-round
+                                     [3 4 1 5]
+                                     (into [] (take 5 (range)))
+                                     0
+                                     0))))))
+  (is (= 40132 (apply * (take 2 (first (knot-hash-round
+                                        (parse-input input)
+                                        (into [] (take 256 (range)))
+                                        0
+                                        0)))))))
 
 (deftest test-parse-input
   (is (= [3 4 1 5] (parse-input "3,4,1,5"))))
+
+(deftest test-parse-input-bytes
+  (is (= [49 44 50 44 51] (parse-input-bytes "1,2,3"))))
+
+(deftest test-condense-hash
+  (is (= [64]
+         (condense-hash [65 27 9 1 4 3 40 50 91 7 6 0 2 5 68 22])))
+  (is (= [64 64]
+         (condense-hash [65 27 9 1 4 3 40 50 91 7 6 0 2 5 68 22
+                         65 27 9 1 4 3 40 50 91 7 6 0 2 5 68 22]))))
+
+(deftest test-to-hex
+  (is (= "4007ff" (to-hex [64 7 255]))))
+
+(deftest test-knot-hash
+  (is (= "a2582a3a0e66e6e86e3812dcb672a272" (knot-hash "")))
+  (is (= "33efeb34ea91902bb2f59c9920caa6cd" (knot-hash "AoC 2017")))
+  (is (= "3efbe78a8d82f29979031a4aa0b16a9d" (knot-hash "1,2,3")))
+  (is (= "63960835bcdc130f0b66d7ff4f6a5a8e" (knot-hash "1,2,4"))))
