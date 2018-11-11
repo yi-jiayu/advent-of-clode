3882387a3284f7466b864a89a09511f7e131c6e3
Jiayu Yi <jiayu.yi@thoughtworks.com>
2018-11-11 16:37:05 +0800

    aoc.year2017.day16: Add solution for part 1

--- src/aoc/year2017/day16.clj
+++ src/aoc/year2017/day16.clj
@@ -3,7 +3,7 @@
 (defn spin
   "Makes `x` programs move from the end to the front, but maintain their order otherwise."
   [x programs]
-  (aoc.core/rotate (- x) programs))
+  (into [] (aoc.core/rotate (- x) programs)))
 
 (defn exchange
   "Makes the programs at positions `a` and `b` swap places."
@@ -20,3 +20,27 @@
   (let [ia (.indexOf programs a)
         ib (.indexOf programs b)]
     (exchange ia ib programs)))
+
+(defn parse-input
+  [input]
+  (map #(case (first %)
+          \s [:s (Integer/parseInt (subs % 1))]
+          \x (let [[a b] (clojure.string/split (subs % 1) #"/")]
+               [:x (Integer/parseInt a) (Integer/parseInt b)])
+          \p [:p (nth % 1) (nth % 3)])
+       (clojure.string/split input #",")))
+
+(defn programs
+  [n]
+  (mapv char (range (int \a) (+ (int \a) n))))
+
+(defn dance
+  [moves programs]
+  (reduce (fn [programs move]
+            (case (first move)
+              :s (spin (second move) programs)
+              :x (exchange (second move) (nth move 2) programs)
+              :p (partner (second move) (nth move 2) programs)))
+          programs
+          moves))
+
--- test/aoc/year2017/day16_test.clj
+++ test/aoc/year2017/day16_test.clj
@@ -1,6 +1,10 @@
 (ns aoc.year2017.day16-test
-  (:require [clojure.test :refer :all])
-  (:require [aoc.year2017.day16 :refer :all]))
+  (:require [clojure.test :refer :all]
+            [clojure.java.io :as io]
+            [aoc.year2017.day16 :refer :all]))
+
+(def example-input "pk/f,x10/12,s13")
+(def input (slurp (io/resource "year2017/day16.txt")))
 
 (deftest spin-test
   (is (= (into [] "cdeab") (spin 3 (into [] "abcde")))))
@@ -10,3 +14,13 @@
 
 (deftest partner-test
   (is (= (into [] "baedc") (partner \e \b (into [] "eabdc")))))
+
+(deftest parse-input-test
+  (is (= [[:p \k \f] [:x 10 12] [:s 13]] (parse-input example-input))))
+
+(deftest programs-test
+  (is (= (into [] "abcde") (programs 5))))
+
+(deftest dance-test
+  (is (= (into [] "baedc") (dance (parse-input "s1,x3/4,pe/b") (into [] "abcde"))))
+  (is (= (into [] "padheomkgjfnblic") (dance (parse-input input) (programs 16)))))
