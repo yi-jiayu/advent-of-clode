0d7ed54e688e03d9bc5b198953c49e0405d5e7e3
Jiayu Yi <jiayu.yi@thoughtworks.com>
2018-11-12 21:59:41 +0800

    Update Clojure version to 1.9

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
   :profiles {:dev {:plugins   [[lein-cloverage "1.0.13"] [lein-cljfmt "0.6.1"]]
                    :cloverage {:test-ns-regex [#"^((?!slow-test).)*$"]}}}
   :test-selectors {:default (complement :slow)})
