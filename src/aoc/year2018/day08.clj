c43dc3bfd4e03cce6978de837f801b499d44e5f3
Jiayu Yi <jiayu.yi@thoughtworks.com>
2018-12-08 14:46:29 +0800

    aoc.year2018.day08: Add solution for part 2

--- src/aoc/year2018/day08.clj
+++ src/aoc/year2018/day08.clj
@@ -25,3 +25,30 @@
                 (+ length child-length)
                 (drop child-length data)
                 (dec remaining-children)))))))
+
+(defn sum-metadata-indexes
+  [child-sums metadata]
+  (->> metadata
+       (map dec)
+       (map (partial get child-sums))
+       (remove nil?)
+       (apply +)))
+
+(defn sum-metadata-complicated
+  "Sums the metadata of `node` and all its child nodes using the more complicated algorithm."
+  ([node]
+   (sum-metadata-complicated (first node) (second node) (drop 2 node)))
+  ([num-children metadata-length data]
+   (if (zero? num-children)
+     [(apply + (take metadata-length data)) (+ 2 metadata-length)]
+     (loop [child-sums []
+            length 0
+            data data
+            remaining-children num-children]
+       (if (zero? remaining-children)
+         [(sum-metadata-indexes child-sums (take metadata-length data)) (+ 2 length metadata-length)]
+         (let [[child-sum child-length] (sum-metadata-complicated data)]
+           (recur (conj child-sums child-sum)
+                  (+ length child-length)
+                  (drop child-length data)
+                  (dec remaining-children))))))))
--- test/aoc/year2018/day08_test.clj
+++ test/aoc/year2018/day08_test.clj
@@ -16,3 +16,14 @@
   (is (= 101 (sum-metadata [1 1 0 1 99 2])))
   (is (= 138 (sum-metadata [2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2])))
   (is (= 40848 (sum-metadata (parse-tree input)))))
+
+(deftest sum-metadata-indexes-test
+  (is (= 0 (sum-metadata-indexes [99] [2])))
+  (is (= 66 (sum-metadata-indexes [33 0] [1 1 2]))))
+
+(deftest sum-metadata-complicated-test
+  (is (= [99 3] (sum-metadata-complicated [0 1 99])))
+  (is (= [33 5] (sum-metadata-complicated [0 3 10 11 12])))
+  (is (= [0 6] (sum-metadata-complicated [1 1 0 1 99 2])))
+  (is (= [66 16] (sum-metadata-complicated [2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2])))
+  (is (= [34466 15824] (sum-metadata-complicated (parse-tree input)))))
