4d8edb4dddffb9d7edac96160c010f7362c616dc
Jiayu Yi <jiayu.yi@thoughtworks.com>
2018-12-11 00:05:03 +0800

    Convert project to polyglot project

--- project.clj
+++ project.clj
@@ -3,9 +3,16 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :source-paths ["src/clojure"]
+  :java-source-paths ["src/java" "test/java"]
+  :test-paths ["test/clojure"]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/math.combinatorics "0.1.4"]]
-  :profiles {:dev {:plugins   [[lein-cloverage "1.0.13"]
-                               [lein-cljfmt "0.6.1"]]
-                   :cloverage {:test-ns-regex [#"^((?!test-skip-coverage).)*$"]}}}
+  :profiles {:dev {:plugins      [[lein-cloverage "1.0.13"]
+                                  [lein-cljfmt "0.6.1"]]
+                   :cloverage    {:test-ns-regex [#"^((?!test-skip-coverage).)*$"]}
+                   :dependencies [[org.junit.platform/junit-platform-console-standalone "1.3.2"]
+                                  [org.hamcrest/hamcrest-all "1.3"]]
+                   :aliases      {"run-junit" ["trampoline" "run" "-m" "org.junit.platform.console.ConsoleLauncher"
+                                               "--scan-class-path"]}}}
   :test-selectors {:default (complement :slow)})
