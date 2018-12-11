3fd0200220810d6ded48f875da4df93b048e4f01
Jiayu Yi <jiayu.yi@thoughtworks.com>
2018-12-11 11:24:17 +0800

    Delegate Java testing and coverage to Gradle

--- project.clj
+++ project.clj
@@ -8,11 +8,9 @@
   :test-paths ["test/clojure"]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/math.combinatorics "0.1.4"]]
-  :profiles {:dev {:plugins      [[lein-cloverage "1.0.13"]
-                                  [lein-cljfmt "0.6.1"]]
-                   :cloverage    {:test-ns-regex [#"^((?!test-skip-coverage).)*$"]}
-                   :dependencies [[org.junit.platform/junit-platform-console-standalone "1.3.2"]
-                                  [org.hamcrest/hamcrest-all "1.3"]]
-                   :aliases      {"run-junit" ["trampoline" "run" "-m" "org.junit.platform.console.ConsoleLauncher"
-                                               "--scan-class-path"]}}}
+  :profiles {:dev    {:plugins      [[lein-cloverage "1.0.13"]
+                                     [lein-cljfmt "0.6.1"]]
+                      :cloverage    {:test-ns-regex [#"^((?!test-skip-coverage).)*$"]}
+                      :dependencies [[org.junit.platform/junit-platform-console-standalone "1.3.2"]
+                                     [org.hamcrest/hamcrest-all "1.3"]]}}
   :test-selectors {:default (complement :slow)})
