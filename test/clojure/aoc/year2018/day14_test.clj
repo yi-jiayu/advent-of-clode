1ae871c7fb1e73bb9469e10cbab428fd1f953b87
Jiayu Yi <jiayu.yi@thoughtworks.com>
2018-12-14 16:59:06 +0800

    year2018.day14: Skip actual input when calculating coverage

--- test/clojure/aoc/year2018/day14_test.clj
+++ test/clojure/aoc/year2018/day14_test.clj
@@ -16,12 +16,10 @@
   (is (= "5158916779" (apply str (last-m-recipes-after-making-n 9 10))))
   (is (= "0124515891" (apply str (last-m-recipes-after-making-n 5 10))))
   (is (= "9251071085" (apply str (last-m-recipes-after-making-n 18 10))))
-  (is (= "5941429882" (apply str (last-m-recipes-after-making-n 2018 10))))
-  (is (= "2810862211" (apply str (last-m-recipes-after-making-n 509671 10)))))
+  (is (= "5941429882" (apply str (last-m-recipes-after-making-n 2018 10)))))
 
 (deftest scoreboard-until-specfic-recipe-sequence-test
   (is (= 9 (scoreboard-until-specfic-recipe-sequence [5 1 5 8 9])))
   (is (= 5 (scoreboard-until-specfic-recipe-sequence [0 1 2 4 5])))
   (is (= 18 (scoreboard-until-specfic-recipe-sequence [9 2 5 1 0])))
-  (is (= 2018 (scoreboard-until-specfic-recipe-sequence [5 9 4 1 4])))
-  (is (= 20227889 (scoreboard-until-specfic-recipe-sequence [5 0 9 6 7 1]))))
+  (is (= 2018 (scoreboard-until-specfic-recipe-sequence [5 9 4 1 4]))))
---
+++ test/clojure/aoc/year2018/day_14_test_skip_coverage.clj
@@ -0,0 +1,9 @@
+(ns aoc.year2018.day-14-test-skip-coverage
+  (:require [clojure.test :refer :all]
+            [aoc.year2018.day14 :refer :all]))
+
+(deftest last-m-recipes-after-making-n-test
+  (is (= "2810862211" (apply str (last-m-recipes-after-making-n 509671 10)))))
+
+(deftest scoreboard-until-specfic-recipe-sequence-test
+  (is (= 20227889 (scoreboard-until-specfic-recipe-sequence [5 0 9 6 7 1]))))
