49fae7d7a1069b95c6f449bdc1eb2912e1a60036
Jiayu Yi <jiayu.yi@thoughtworks.com>
2018-12-16 16:04:31 +0800

    Add test.check as a dev dependency

--- project.clj
+++ project.clj
@@ -8,9 +8,10 @@
   :test-paths ["test/clojure"]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/math.combinatorics "0.1.4"]]
-  :profiles {:dev    {:plugins      [[lein-cloverage "1.0.13"]
-                                     [lein-cljfmt "0.6.1"]]
-                      :cloverage    {:test-ns-regex [#"^((?!test-skip-coverage).)*$"]}
-                      :dependencies [[org.junit.platform/junit-platform-console-standalone "1.3.2"]
-                                     [org.hamcrest/hamcrest-all "1.3"]]}}
+  :profiles {:dev {:plugins      [[lein-cloverage "1.0.13"]
+                                  [lein-cljfmt "0.6.1"]]
+                   :cloverage    {:test-ns-regex [#"^((?!test-skip-coverage).)*$"]}
+                   :dependencies [[org.clojure/test.check "0.9.0"]
+                                  [org.junit.platform/junit-platform-console-standalone "1.3.2"]
+                                  [org.hamcrest/hamcrest-all "1.3"]]}}
   :test-selectors {:default (complement :slow)})
