9f13ee0bc23672cfec861175cbbbf6ea8e3130fa
Jiayu Yi <jiayu.yi@thoughtworks.com>
2018-12-16 21:08:20 +0800

    year2018.day16: Add specs for all instructions
    To avoid reimplementing the code under test in our specs, the :fn spec
    is no longer used.
    
    addi, mulr and muli all use arbitrary-precision arithmetic.

--- src/clojure/aoc/year2018/day16.clj
+++ src/clojure/aoc/year2018/day16.clj
@@ -6,72 +6,118 @@
 
 (defmacro binoprr
   [name op doc]
-  (list 'defn name
-        doc
-        '[regs a b c]
-        (list 'assoc 'regs 'c (list op '(regs a) '(regs b)))))
-
+  (list 'do
+        (list 'defn name
+              doc
+              '[regs a b c]
+              (list 'assoc 'regs 'c (list op '(regs a) '(regs b))))
+        (list 's/fdef name
+              :args '(s/cat :regs ::registers? :a ::register? :b ::register? :c ::register?)
+              :ret ::registers?)))
 
 (defmacro binopir
   [name op doc]
-  (list 'defn name
-        doc
-        '[regs a b c]
-        (list 'assoc 'regs 'c (list op 'a '(regs b)))))
+  (list 'do
+        (list 'defn name
+              doc
+              '[regs a b c]
+              (list 'assoc 'regs 'c (list op 'a '(regs b))))
+        (list 's/fdef name
+              :args '(s/cat :regs ::registers? :a integer? :b ::register? :c ::register?)
+              :ret ::registers?)))
 
 (defmacro binopri
   [name op doc]
-  (list 'defn name
-        doc
-        '[regs a b c]
-        (list 'assoc 'regs 'c (list op '(regs a) 'b))))
+  (list 'do
+        (list 'defn name
+              doc
+              '[regs a b c]
+              (list 'assoc 'regs 'c (list op '(regs a) 'b)))
+        (list 's/fdef name
+              :args '(s/cat :regs ::registers? :a ::register? :b integer? :c ::register?)
+              :ret ::registers?)))
+
+(defmacro unopr
+  [name op doc]
+  (list 'do
+        (list 'defn name
+              doc
+              '[regs a _ c]
+              (list 'assoc 'regs 'c (list op '(regs a))))
+        (list 's/fdef name
+              :args '(s/cat :regs ::registers? :a ::register? :b any? :c ::register?)
+              :ret ::registers?)))
+
+(defmacro unopi
+  [name op doc]
+  (list 'do
+        (list 'defn name
+              doc
+              '[regs a _ c]
+              (list 'assoc 'regs 'c (list op 'a)))
+        (list 's/fdef name
+              :args '(s/and (s/cat :regs ::registers? :a integer? :b any? :c ::register?))
+              :ret ::registers?)))
 
 (binoprr addr +'
-  "addr (add register) stores into register C the result of adding register A and register B.")
-(s/fdef addr
-        :args (s/and (s/cat :regs ::registers? :a ::register? :b ::register? :c ::register?))
-        :ret ::registers?
-        :fn (fn [{{:keys [regs a b c]} :args ret :ret}]
-              (let [expected (+' (regs a) (regs b))]
-                (= (assoc regs c expected) ret))))
+  "addr (add register) stores into register C the result of adding register A
+  and register B.")
 
-(binopri addi +
-  "addi (add immediate) stores into register C the result of adding register A and value B.")
+(binopri addi +'
+  "addi (add immediate) stores into register C the result of adding register A
+  and value B.")
 
-(binoprr mulr *
-  "mulr (multiply register) stores into register C the result of multiplying register A and register B.")
-(binopri muli *
-  "muli (multiply immediate) stores into register C the result of multiplying register A and value B.")
+(binoprr mulr *'
+  "mulr (multiply register) stores into register C the result of multiplying
+  register A and register B.")
+
+(binopri muli *'
+  "muli (multiply immediate) stores into register C the result of multiplying
+  register A and value B.")
 
 (binoprr banr bit-and
-  "(bitwise AND register) stores into register C the result of the bitwise AND of register A and register B.")
+  "(bitwise AND register) stores into register C the result of the bitwise AND
+  of register A and register B.")
+
 (binopri bani bit-and
-  "(bitwise AND immediate) stores into register C the result of the bitwise AND of register A and value B.")
+  "(bitwise AND immediate) stores into register C the result of the bitwise AND
+  of register A and value B.")
 
 (binoprr borr bit-or
-  "borr (bitwise OR register) stores into register C the result of the bitwise OR of register A and register B.")
-(binopri bori bit-or
-  "bori (bitwise OR immediate) stores into register C the result of the bitwise OR of register A and value B.")
+  "borr (bitwise OR register) stores into register C the result of the bitwise
+  OR of register A and register B.")
 
-(binopri setr (fn [a _] a)
-  "setr (set register) copies the contents of register A into register C. (Input B is ignored.)")
-(binopir seti (fn [a _] a)
+(binopri bori bit-or
+  "bori (bitwise OR immediate) stores into register C the result of the bitwise
+  OR of register A and value B.")
+
+(unopr setr identity
+  "setr (set register) copies the contents of register A into register C.
+  (Input B is ignored.)")
+
+(unopi seti identity
   "seti (set immediate) stores value A into register C. (Input B is ignored.)")
 
 (binopir gtir (fn [a b] (if (> a b) 1 0))
-  "gtir (greater-than immediate/register) sets register C to 1 if value A is greater than register B. Otherwise, register C is set to 0.")
+  "gtir (greater-than immediate/register) sets register C to 1 if value A is
+  greater than register B. Otherwise, register C is set to 0.")
 
 (binopri gtri (fn [a b] (if (> a b) 1 0))
-  "gtri (greater-than register/immediate) sets register C to 1 if register A is greater than value B. Otherwise, register C is set to 0.")
+  "gtri (greater-than register/immediate) sets register C to 1 if register A is
+  greater than value B. Otherwise, register C is set to 0.")
 
 (binoprr gtrr (fn [a b] (if (> a b) 1 0))
-  "gtrr (greater-than register/register) sets register C to 1 if register A is greater than register B. Otherwise, register C is set to 0.")
+  "gtrr (greater-than register/register) sets register C to 1 if register A is
+  greater than register B. Otherwise, register C is set to 0.")
 
 (binopir eqir (fn [a b] (if (= a b) 1 0))
-  "eqir (equal immediate/register) sets register C to 1 if value A is equal to register B. Otherwise, register C is set to 0.")
+  "eqir (equal immediate/register) sets register C to 1 if value A is equal to
+  register B. Otherwise, register C is set to 0.")
 
 (binopri eqri (fn [a b] (if (= a b) 1 0))
-  "eqri (equal register/immediate) sets register C to 1 if register A is equal to value B. Otherwise, register C is set to 0.")
+  "eqri (equal register/immediate) sets register C to 1 if register A is equal
+  to value B. Otherwise, register C is set to 0.")
 
 (binoprr eqrr (fn [a b] (if (= a b) 1 0))
-  "eqrr (equal register/register) sets register C to 1 if register A is equal to register B. Otherwise, register C is set to 0.")
+  "eqrr (equal register/register) sets register C to 1 if register A is equal
+  to register B. Otherwise, register C is set to 0.")
--- test/clojure/aoc/year2018/day16_test.clj
+++ test/clojure/aoc/year2018/day16_test.clj
@@ -8,8 +8,53 @@
 (deftest addi-test
   (is (= [1 3 0 0] (addi [1 0 0 0] 0 2 1))))
 
+(deftest mulr-test
+  (is (= [2 3 6 0] (mulr [2 3 0 0] 0 1 2))))
+
+(deftest muli-test
+  (is (= [2 6 0 0] (muli [2 0 0 0] 0 3 1))))
+
 (deftest banr-test
   (is (= [3 6 2 0] (banr [3 6 0 0] 0 1 2))))
 
 (deftest bani-test
-  (is (= [3 0 2 0] (bani [3 0 0 0] 0 6 2))))
+  (is (= [3 2 0 0] (bani [3 0 0 0] 0 6 1))))
+
+(deftest borr-test
+  (is (= [3 6 7 0] (borr [3 6 0 0] 0 1 2))))
+
+(deftest bori-test
+  (is (= [3 7 0 0] (bori [3 0 0 0] 0 6 1))))
+
+(deftest setr-test
+  (is (= [3 3 0 0] (setr [3 0 0 0] 0 nil 1))))
+
+(deftest seti-test
+  (is (= [0 3 0 0] (seti [0 0 0 0] 3 nil 1))))
+
+(deftest gtir-test
+  (is (= [0 1 1 0] (gtir [0 1 2 0] 2 1 2)))
+  (is (= [0 1 0 0] (gtir [0 1 2 0] 1 1 2)))
+  (is (= [0 1 0 0] (gtir [0 1 2 0] 0 1 2))))
+
+(deftest gtri-test
+  (is (= [2 0 1 0] (gtri [2 0 2 0] 0 1 2)))
+  (is (= [2 0 0 0] (gtri [2 0 2 0] 0 2 2)))
+  (is (= [2 0 0 0] (gtri [2 0 2 0] 0 3 2))))
+
+(deftest gtrr-test
+  (is (= [2 1 1 0] (gtrr [2 1 2 0] 0 1 2)))
+  (is (= [2 2 0 0] (gtrr [2 2 2 0] 0 1 2)))
+  (is (= [2 3 0 0] (gtrr [2 3 2 0] 0 1 2))))
+
+(deftest eqir-test
+  (is (= [0 1 1 0] (eqir [0 1 2 0] 1 1 2)))
+  (is (= [0 1 0 0] (eqir [0 1 2 0] 2 1 2))))
+
+(deftest eqri-test
+  (is (= [0 1 1 0] (eqri [0 1 2 0] 1 1 2)))
+  (is (= [0 1 0 0] (eqri [0 1 2 0] 1 2 2))))
+
+(deftest eqrr-test
+  (is (= [1 1 1 0] (eqrr [1 1 2 0] 0 1 2)))
+  (is (= [1 2 0 0] (eqrr [1 2 2 0] 0 1 2))))
