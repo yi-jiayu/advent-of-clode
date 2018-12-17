adf7e77c0fd3acfa32881b773af37eabcccf5344
Jiayu Yi <jiayu.yi@thoughtworks.com>
2018-12-18 00:23:22 +0800

    year2018.day15: Follow algorithm in problem statement more closely

--- src/clojure/aoc/year2018/day15.clj
+++ src/clojure/aoc/year2018/day15.clj
@@ -3,9 +3,18 @@
             [aoc.core :refer [adjacent]])
   (:import (clojure.lang PersistentQueue)))
 
+(def ^:dynamic *debug* false)
+
 (def starting-hp 200)
 
-(defrecord BattleState [map units])
+(defrecord Battle [cavern units])
+(defrecord Unit [type hp])
+
+(defmethod print-method Battle [battle ^java.io.Writer w]
+  (.write w (str "(->Battle" (:cavern battle) (:units battle) ")")))
+
+(defmethod print-method Unit [unit ^java.io.Writer w]
+  (.write w (str "(->Unit " (:type unit) " " (:hp unit) ")")))
 
 (defn enumerate2d
   "Returns a lazy sequence consisting of [row col elem] for each element in matrix."
@@ -19,32 +28,71 @@
   (->> input
        string/trim
        string/split-lines
-       (mapv string/trim)))
+       (map string/trim)
+       (mapv (partial into []))))
 
 (defn extract-units
   [cavern]
-  (let [[elves goblins] (reduce (fn [[elves goblins] [row col c]]
-                                  (case c
-                                    \E [(assoc elves [row col] starting-hp) goblins]
-                                    \G [elves (assoc goblins [row col] starting-hp)]
-                                    [elves goblins]))
-                                [{} {}]
-                                (enumerate2d cavern))]
-    {\E elves
-     \G goblins}))
+  (reduce (fn [units [row col c]]
+            (case c
+              \E (assoc units [row col] (->Unit \E starting-hp))
+              \G (assoc units [row col] (->Unit \G starting-hp))
+              units))
+          {}
+          (enumerate2d cavern)))
 
-(defn initialise-battle-state
+(defn parse-input
   [input]
   (let [map (parse-cavern-map input)
         units (extract-units map)]
-    (->BattleState map units)))
+    (->Battle map units)))
+
+(defn identify-targets
+  "Identify all possible targets for unit at `position`. If there are no
+  targets remaining, returns nil."
+  [{units :units} position]
+  (let [unit-type (get-in units [position :type])
+        targets (keep (fn [[pos {type :type}]] (when (not= unit-type type) pos)) units)]
+    (when (not (empty? targets))
+      targets)))
+
+(defn identify-open-squares
+  "Identifies all open squares in range of each target in `targets`. If there
+  are no open squares, returns nil."
+  [{cavern :cavern} targets]
+  (let [adjacent-squares (mapcat adjacent targets)
+        open-squares (filter (fn [square] (= \. (get-in cavern square))) adjacent-squares)]
+    (when (not (empty? open-squares))
+      (into #{} open-squares))))
+
+(defn choose-next-step
+  "Returns the next step to take to reach the chosen open square in `open-squares`. If
+  none of the open squares are reachable, returns nil."
+  [{cavern :cavern} position open-squares]
+  (let [choices (sort-by first (for [adjacent (adjacent position)
+                                     :when (= \. (get-in cavern adjacent))]
+                                 [adjacent adjacent]))]
+    (loop [open (apply conj PersistentQueue/EMPTY choices)
+           closed #{}]
+      (when-not (empty? open)
+        (let [[choice curr] (peek open)]
+          (if (open-squares curr)
+            choice
+            (let [open (pop open)
+                  available-moves (->> curr
+                                       adjacent
+                                       (remove closed)
+                                       (keep (fn [adj] (when (= \. (get-in cavern adj)) [choice adj])))
+                                       (sort-by first))]
+              (recur (apply conj open available-moves)
+                     (conj closed curr)))))))))
 
 (defn enemy-in-range
   "If a unit at `start-pos` is in range of at least one enemy unit represented
   by `target`, returns the position of the enemy unit in range with the lowest
   hit points."
   [battle start-pos target]
-  (let [cavern (:map battle)
+  (let [cavern (:cavern battle)
         enemy-positions (sort-by first (keep (fn [[pos tile]] (if (= target tile) pos))
                                              (for [adjacent (adjacent start-pos)]
                                                [adjacent (get-in cavern adjacent)])))]
@@ -57,6 +105,7 @@
         pos))))
 
 (defn find-target
+  "Returns the next tile the unit should move to."
   [cavern start-pos target]
   (let [choices (sort-by first (for [adjacent (adjacent start-pos)
                                      :when (= \. (get-in cavern adjacent))]
@@ -68,8 +117,65 @@
             adjacent (sort-by first (for [adjacent (adjacent curr)
                                           :when (not (closed adjacent))]
                                       [adjacent (get-in cavern adjacent)]))]
-        (if (first (filter (fn [[_ tile]] (= target tile)) adjacent))
-          [choice curr]
-          (let [available-moves (keep (fn [[pos tile]] (if (= \. tile) [choice pos])) adjacent)]
-            (recur (apply conj open available-moves)
-                   (conj closed curr))))))))
+        (if (empty? open)
+          nil
+          (if (first (filter (fn [[_ tile]] (= target tile)) adjacent))
+            [choice curr]
+            (let [available-moves (keep (fn [[pos tile]] (if (= \. tile) [choice pos])) adjacent)]
+              (recur (apply conj open available-moves)
+                     (conj closed curr)))))))))
+
+(defn move-unit
+  "Moves the unit at pos to new pos."
+  [{:keys [cavern units]} pos new-pos]
+  (let [unit (get-in cavern pos)
+        faction (units unit)
+        hp (faction pos)]
+    (->Battle (-> cavern
+                  (assoc-in pos \.)
+                  (assoc-in new-pos unit))
+              (assoc units unit (-> faction
+                                    (dissoc pos)
+                                    (assoc new-pos hp))))))
+
+(defn attack
+  "`attacker` deals damage equal to `attack-power` to `defender`. `defender` is
+  removed from the battle if its remaining hit points are 0 or fewer."
+  [{:keys [cavern units]} attacker attack-power defender]
+  (let [defender-unit-type (get-in cavern defender)
+        defender-faction (units defender-unit-type)
+        defender-hp-before (defender-faction defender)
+        defender-hp-after (- defender-hp-before attack-power)
+        [cavern defender-faction-after] (if (>= 0 defender-hp-after)
+                                          [(assoc-in cavern defender \.) (dissoc defender-faction defender)]
+                                          [cavern (assoc defender-faction defender defender-hp-after)])]
+    (->Battle cavern (assoc units defender-unit-type defender-faction-after))))
+
+(defn move-if-enemy-not-in-range
+  [{:keys [cavern _] :as battle} pos target]
+  (if (not (enemy-in-range battle pos target))
+    (if-let [[destination _] (find-target cavern pos target)]
+      [(move-unit battle pos destination) destination]
+      [battle pos])
+    [battle pos]))
+
+(defn attack-if-enemy-in-range
+  [[battle pos] target]
+  (if-let [target-pos (enemy-in-range battle pos target)]
+    (attack battle pos 3 target-pos)
+    battle))
+
+(defn take-turn
+  "The unit at `pos` takes a turn."
+  [{:keys [cavern _] :as battle} pos]
+  (let [faction (get-in cavern pos)
+        target (case faction \E \G \G \E)]
+    (-> battle
+        (move-if-enemy-not-in-range pos target)
+        (attack-if-enemy-in-range target))))
+
+;(defn simulate-battle
+;  "Simulates a battle from start to finish and returns the outcome."
+;  [{:keys [cavern units] :as battle}]
+;  (loop [all-units (sort (mapcat keys (vals units)))]
+;    (reduce (fn [battle unit]))))
--- test/clojure/aoc/year2018/day15_test.clj
+++ test/clojure/aoc/year2018/day15_test.clj
@@ -17,11 +17,11 @@
             [2 2 9]] (enumerate2d matrix)))))
 
 (deftest parse-cavern-map-test
-  (is (= ["#######"
-          "#.G.E.#"
-          "#E.G.E#"
-          "#.G.E.#"
-          "#######"]
+  (is (= [[\# \# \# \# \# \# \#]
+          [\# \. \G \. \E \. \#]
+          [\# \E \. \G \. \E \#]
+          [\# \. \G \. \E \. \#]
+          [\# \# \# \# \# \# \#]]
          (parse-cavern-map "#######
                             #.G.E.#
                             #E.G.E#
@@ -29,38 +29,100 @@
                             #######"))))
 
 (deftest extract-units-test
-  (is (= {\E (zipmap [[1 4] [2 1] [2 5] [3 4]] (repeat starting-hp))
-          \G (zipmap [[1 2] [2 3] [3 2]] (repeat starting-hp))}
+  (is (= {[1 2] (->Unit \G 200)
+          [1 4] (->Unit \E 200)
+          [2 1] (->Unit \E 200)
+          [2 3] (->Unit \G 200)
+          [2 5] (->Unit \E 200)
+          [3 2] (->Unit \G 200)
+          [3 4] (->Unit \E 200)}
          (extract-units ["#######"
                          "#.G.E.#"
                          "#E.G.E#"
                          "#.G.E.#"
                          "#######"]))))
-(deftest initialise-battle-state-test
-  (is (= (->BattleState ["#######"
-                         "#.G.E.#"
-                         "#E.G.E#"
-                         "#.G.E.#"
-                         "#######"]
-                        {\E (zipmap [[1 4] [2 1] [2 5] [3 4]] (repeat starting-hp))
-                         \G (zipmap [[1 2] [2 3] [3 2]] (repeat starting-hp))})
-         (initialise-battle-state "#######
+(deftest parse-input-test
+  (is (= (->Battle [[\# \# \# \# \# \# \#]
+                    [\# \. \G \. \E \. \#]
+                    [\# \E \. \G \. \E \#]
+                    [\# \. \G \. \E \. \#]
+                    [\# \# \# \# \# \# \#]]
+                   {[1 2] (->Unit \G 200)
+                    [1 4] (->Unit \E 200)
+                    [2 1] (->Unit \E 200)
+                    [2 3] (->Unit \G 200)
+                    [2 5] (->Unit \E 200)
+                    [3 2] (->Unit \G 200)
+                    [3 4] (->Unit \E 200)})
+         (parse-input "#######
                                    #.G.E.#
                                    #E.G.E#
                                    #.G.E.#
                                    #######"))))
 
-(deftest enemy-in-range-test
-  (let [battle (->BattleState ["G...."
-                               "..G.."
-                               "..EG."
-                               "..G.."
-                               "...G."]
-                              {\E {[2 2] starting-hp}
-                               \G {[0 0] 9 [1 2] 4 [2 3] 2 [3 2] 2 [4 3] 1}})]
-    (is (= nil (enemy-in-range battle [0 0] \E)))
-    (is (= [2 2] (enemy-in-range battle [1 2] \E)))
-    (is (= [2 3] (enemy-in-range battle [2 2] \G)))))
+(deftest identify-targets-test
+  (let [units {[1 1] (->Unit \E 200)
+               [1 4] (->Unit \G 200)
+               [3 2] (->Unit \G 200)
+               [3 5] (->Unit \G 200)}]
+    (is (= [[1 4] [3 2] [3 5]] (identify-targets {:units units} [1 1]))))
+  (testing "no targets remaining"
+    (let [units {[1 1] (->Unit \E 200)}]
+      (is (= nil (identify-targets {:units units} [1 1]))))))
+
+(deftest identify-open-squares-test
+  (let [cavern [[\# \# \# \# \# \# \#]
+                [\# \E \. \. \G \. \#]
+                [\# \. \. \. \# \. \#]
+                [\# \. \G \. \# \G \#]
+                [\# \# \# \# \# \# \#]]]
+    (is (= #{[1 3] [1 5] [2 2] [2 5] [3 1] [3 3]}
+           (identify-open-squares {:cavern cavern} [[1 4] [3 2] [3 5]]))))
+  (testing "no open squares"
+    (let [cavern [[\# \# \# \# \# \#]
+                  [\# \E \. \E \G \#]
+                  [\# \. \. \# \G \#]
+                  [\# \# \# \# \# \#]]]
+      (is (= nil
+             (identify-open-squares {:cavern cavern} [[1 4] [2 4]]))))))
+
+(deftest choose-next-step-test
+  (let [cavern ["#######"
+                "#.E...#"
+                "#.....#"
+                "#...G.#"
+                "#######"]]
+    (is (= [1 3] (choose-next-step {:cavern cavern} [1 2] #{[3 3] [2 4] [3 5]}))))
+  (let [cavern ["#######"
+                "#.G...#"
+                "#...E.#"
+                "#.G...#"
+                "#######"]]
+    (is (= [1 4] (choose-next-step {:cavern cavern} [2 4] #{[2 2] [3 3] [1 1]
+                                                            [1 3] [3 1]}))))
+  (testing "should return nil when no targets are reachable"
+    (let [cavern ["#######"
+                  "#E..E.#"
+                  "#...#.#"
+                  "#...#G#"
+                  "#######"]]
+      (is (= nil (choose-next-step {:cavern cavern} [1 1] #{[2 5]}))))))
+
+;(deftest enemy-in-range-test
+;  (let [battle (->Battle ["G...."
+;                          "..G.."
+;                          "..EG."
+;                          "..G.."
+;                          "...G."]
+;                         {[0 0] (->Unit \G 200),
+;                          [1 2] (->Unit \G 200),
+;                          [2 2] (->Unit \E 200),
+;                          [2 3] (->Unit \G 200),
+;                          [3 2] (->Unit \G 200),
+;                          [4 3] (->Unit \G 200)})]
+;    (is (= nil (enemy-in-range battle [0 0] \E)))
+;    (is (= [2 2] (enemy-in-range battle [1 2] \E)))
+;    (is (= [2 3] (enemy-in-range battle [2 2] \G)))))
 
 (deftest find-target-test
   (let [cavern ["#######"
@@ -74,4 +136,73 @@
                 "#.....#"
                 "#...G.#"
                 "#######"]]
-    (is (= [[1 3] [2 4]] (find-target cavern [1 2] \G)))))
+    (is (= [[1 3] [2 4]] (find-target cavern [1 2] \G))))
+  (let [cavern ["#######"
+                "#.G...#"
+                "#...E.#"
+                "#.G...#"
+                "#######"]]
+    (is (= [[1 4] [1 3]] (find-target cavern [2 4] \G))))
+  (testing "should return nil when no targets are reachable"
+    (let [cavern ["#######"
+                  "#E..E.#"
+                  "#...#.#"
+                  "#...#G#"
+                  "#######"]]
+      (is (= nil (find-target cavern [1 1] \G))))))
+
+(deftest move-unit-test
+  (let [battle (->Battle
+                 [[\# \# \# \# \# \# \#]
+                  [\# \. \. \E \. \. \#]
+                  [\# \. \E \. \. \. \#]
+                  [\# \. \. \. \G \. \#]
+                  [\# \# \# \# \# \# \#]]
+                 {\E {[1 3] 200
+                      [2 2] 190}
+                  \G {[3 4] 200}})]
+    (is (= (->Battle
+             [[\# \# \# \# \# \# \#]
+              [\# \. \. \. \E \. \#]
+              [\# \. \E \. \. \. \#]
+              [\# \. \. \. \G \. \#]
+              [\# \# \# \# \# \# \#]]
+             {\E {[1 4] 200
+                  [2 2] 190}
+              \G {[3 4] 200}}) (move-unit battle [1 3] [1 4])))))
+
+;(deftest simulate-battle-test
+;  (let [battle (initialise-battle-state "#######\n#.G...#\n#...EG#\n#.#.#G#\n#..G#E#\n#.....#\n#######")]
+;    (is (= 27730 (simulate-battle battle)))))
+
+(deftest attack-test
+  (let [battle (->Battle [[\G \. \. \. \.]
+                          [\. \. \G \. \.]
+                          [\. \. \E \G \.]
+                          [\. \. \G \. \.]
+                          [\. \. \. \G \.]]
+                         {\E {[2 2] starting-hp}
+                          \G {[0 0] 9 [1 2] 4 [2 3] 2 [3 2] 2 [4 3] 1}})]
+    (is (= (->Battle [[\G \. \. \. \.]
+                      [\. \. \G \. \.]
+                      [\. \. \E \. \.]
+                      [\. \. \G \. \.]
+                      [\. \. \. \G \.]]
+                     {\E {[2 2] starting-hp}
+                      \G {[0 0] 9 [1 2] 4 [3 2] 2 [4 3] 1}})
+           (attack battle [2 2] 3 [2 3])))))
+
+(deftest move-if-enemy-not-in-range-test
+  )
+
+;(deftest take-turn-test
+;  (let [battle (parse-input "#######\n#.E...#\n#.....#\n#...G.#\n#######")]
+;    (is (= (->Battle
+;             [[\# \# \# \# \# \# \#]
+;              [\# \. \. \E \. \. \#]
+;              [\# \. \. \. \. \. \#]
+;              [\# \. \. \. \G \. \#]
+;              [\# \# \# \# \# \# \#]]
+;             {\E {[1 3] 200}
+;              \G {[3 4] 200}})
+;           (take-turn battle [1 2])))))
